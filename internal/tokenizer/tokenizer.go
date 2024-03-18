99baeebe0f7705c89c1e915066503bf79c000f89
lucaSartore <lucasar.com8@gmail.com>
2024-03-18 08:14:23 +0100

    add tokenizer main function

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -1,1 +1,16 @@
 package tokenizer
+
+import "bytes"
+
+func Tokenize(input string) ([]Token, error) {
+
+	inputBuffer := bytes.NewBufferString(input)
+
+	return []Token{}, nil
+}
+
+func TokenizeSingle(input *bytes.Buffer) (Token, error) {
+	// ...
+
+	return Token{}, nil
+}

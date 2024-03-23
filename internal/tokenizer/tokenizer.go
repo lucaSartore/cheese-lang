a48fb781709f4ecf3ff470a1310bafd07d458427
Luca Sartore <lucasar.com8@gmail.com>
2024-03-23 07:30:38 +0100

    backup commit

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -1,17 +1,29 @@
 package tokenizer
 
-import "bytes"
+import (
+	"bytes"
+	"errors"
+)
 
 func Tokenize(input string) ([]Token, error) {
 	// todo: check tat the string is made of only asci characters... utf8 characters may brake this tokenizer
-	inputBuffer := bytes.NewBufferString(input)
-
+	inputBuffer := bytes.NewReader([]byte(input))
+	TokenizeSingle(inputBuffer)
 	return []Token{}, nil
 }
 
-func TokenizeSingle(input *bytes.Buffer) (Token, error) {
-	inputBytes := input.Bytes()
-	return Token{}, nil
+func TokenizeSingle(input *bytes.Reader) (Token, error) {
+
+	inputBytes := []byte(input)
+
+	functionToTry := []func([]byte) (Token, uint){TryReadComment, TryReadDualCharacterToken, TryReadSingleCharacterToken}
+	for function := range functionToTry {
+		token, len := TryReadComment(inputBytes)
+		if len > 0 {
+			input.re
+		}
+	}
+	return MakeToken(NullToken), errors.New("unable to find token that match with: " + string(inputBytes))
 }
 
 func AdvanceWitheSpace(input []byte) uint {
@@ -27,7 +39,7 @@ func AdvanceWitheSpace(input []byte) uint {
 }
 
 // try to match with operators that are made of 2 characters
-func TryReadDualCharacterOperator(input []byte) (Token, uint) {
+func TryReadDualCharacterToken(input []byte) (Token, uint) {
 	if len(input) < 2 {
 		return MakeToken(NullToken), 0
 	}
@@ -45,12 +57,14 @@ func TryReadDualCharacterOperator(input []byte) (Token, uint) {
 		return MakeToken(AndOperator), 2
 	case "||":
 		return MakeToken(OrOperator), 2
+	case "->":
+		return MakeToken(Arrow), 2
 	default:
 		return MakeToken(NullToken), 0
 	}
 }
 
-func TryReadSingleCHaracterOperator(input []byte) (Token, uint) {
+func TryReadSingleCharacterToken(input []byte) (Token, uint) {
 	if len(input) == 0 {
 		return MakeToken(NullToken), 0
 	}
@@ -75,7 +89,41 @@ func TryReadSingleCHaracterOperator(input []byte) (Token, uint) {
 		return MakeToken(NotOperator), 1
 	case "^":
 		return MakeToken(ExorOperator), 1
+	case "(":
+		return MakeToken(OpenBracket), 1
+	case ")":
+		return MakeToken(CloseBracket), 1
+	case "{":
+		return MakeToken(OpenCurlyBracket), 1
+	case "}":
+		return MakeToken(CloseCurlyBracket), 1
+	case ",":
+		return MakeToken(Comma), 1
+	case ";":
+		return MakeToken(SemiColon), 1
 	default:
 		return MakeToken(NotOperator), 1
 	}
+}
+
+func TryReadComment(input []byte) (Token, uint) {
+
+	if len(input) == 0 {
+		return MakeToken(NullToken), 0
+	}
+
+	text := string(input[0:1])
+
+	if text != "\\" {
+		return MakeToken(NullToken), 0
+	}
+	count := uint(2)
+
+	for c := range text[2:] {
+		count++
+		if c == '\n' {
+			break
+		}
+	}
+	return MakeTokenWithMessage(Comment, string(text[2:count])), count
 }

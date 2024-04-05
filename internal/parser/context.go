3e4acf8a9c703560f5385d5720ec04bc8e6f69ee
Luca Sartore <lucasar.com8@gmail.com>
2024-04-05 11:43:06 +0200

    Add context method

--- internal/parser/context.go
+++ internal/parser/context.go
@@ -2,5 +2,30 @@ package parser
 
 type Context struct {
 	Functions map[string]Function
-	Variables map[string]VariableType
+	Variables map[string]Variable
+}
+
+func MakeContext() Context {
+	return Context{
+		Functions: make(map[string]Function),
+		Variables: make(map[string]Variable),
+	}
+}
+
+func (c *Context) AddFunction(f Function) {
+	c.Functions[f.Name] = f
+}
+
+func (c *Context) GetFunction(name string) (Function, bool) {
+	f, ok := c.Functions[name]
+	return f, ok
+}
+
+func (c *Context) AddVariable(t Variable) {
+	c.Variables[t.Name] = t
+}
+
+func (c *Context) GetVariable(name string) (Variable, bool) {
+	v, ok := c.Variables[name]
+	return v, ok
 }

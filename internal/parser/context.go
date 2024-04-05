31e73d36b98225f093de315d8a558a7a539814bb
Luca Sartore <lucasar.com8@gmail.com>
2024-04-05 11:45:22 +0200

    add getter functions for global/local context

--- internal/parser/context.go
+++ internal/parser/context.go
@@ -16,16 +16,32 @@ func (c *Context) AddFunction(f Function) {
 	c.Functions[f.Name] = f
 }
 
-func (c *Context) GetFunction(name string) (Function, bool) {
+func (c *Context) GetFunction(name string) (*Function, bool) {
 	f, ok := c.Functions[name]
-	return f, ok
+	return &f, ok
 }
 
 func (c *Context) AddVariable(t Variable) {
 	c.Variables[t.Name] = t
 }
 
-func (c *Context) GetVariable(name string) (Variable, bool) {
+func (c *Context) GetVariable(name string) (*Variable, bool) {
 	v, ok := c.Variables[name]
+	return &v, ok
+}
+
+func GetVariable(localContext *Context, globalContext *Context, name string) (*Variable, bool) {
+	v, ok := localContext.GetVariable(name)
+	if !ok {
+		v, ok = globalContext.GetVariable(name)
+	}
 	return v, ok
+}
+
+func GetFunction(localContext *Context, globalContext *Context, name string) (*Function, bool) {
+	f, ok := localContext.GetFunction(name)
+	if !ok {
+		f, ok = globalContext.GetFunction(name)
+	}
+	return f, ok
 }

fddf0c9dfd1cc26b11138c7dc2a38b5d11da8934
lucaSartore <lucasar.com8@gmail.com>
2024-04-23 20:48:31 +0200

    Add code expression evaluation

--- internal/parser/expressions/code_expression.go
+++ internal/parser/expressions/code_expression.go
@@ -1,7 +1,29 @@
 package expressions
 
+import (
+	"cheese-lang/internal/parser"
+)
+
 // a code expression is a list of expressions that are evaluated in order
 
 type CodeExpression struct {
 	Expressions []Expression
+}
+
+func (ce *CodeExpression) Evaluate(globalContext *parser.Context, localContext *parser.Context) (ExpressionResult, error) {
+	lastResult := VoidExpressionResult
+
+	for _, exp := range ce.Expressions {
+
+		lastResult, err := exp.Evaluate(globalContext, localContext)
+
+		if err != nil {
+			return NullExpressionResult, nil
+		}
+
+		if lastResult.Return != nil || lastResult.Brake != nil {
+			return lastResult, nil
+		}
+	}
+	return lastResult, nil
 }

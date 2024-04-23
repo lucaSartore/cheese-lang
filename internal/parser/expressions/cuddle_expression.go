34662d0671e8aac6037f9ed56f43296541bc9311
lucaSartore <lucasar.com8@gmail.com>
2024-04-23 20:57:51 +0200

    Add cuddle expression evaluation

--- internal/parser/expressions/cuddle_expression.go
+++ internal/parser/expressions/cuddle_expression.go
@@ -1,3 +1,31 @@
 package expressions
 
+import (
+	"cheese-lang/internal/parser"
+)
+
 // CuddleExpression is the basic loop control flow of the language
+
+type CuddleExpression struct {
+	codeInside CodeExpression
+}
+
+func (ce *CuddleExpression) Evaluate(globalContext *parser.Context, localContext *parser.Context) (ExpressionResult, error) {
+
+	for {
+		result, err := ce.codeInside.Evaluate(globalContext, localContext)
+
+		if err != nil {
+			return NullExpressionResult, err
+		}
+
+		if result.Return != nil {
+			return result, nil
+		}
+
+		if result.Brake != nil {
+			return result, nil
+		}
+	}
+
+}

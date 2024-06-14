9450bbda6ffbcf9a35a01480311ebc82de567d89
Luca Sartore <lucasar.com8@gmail.com>
2024-06-14 19:15:01 +0200

    add literal expression

--- internal/parser/expressions/literal_expression.go
+++ internal/parser/expressions/literal_expression.go
@@ -1,1 +1,13 @@
 package expressions
+
+import (
+	"cheese-lang/internal/parser"
+)
+
+type LiteralExpression struct {
+	Literal parser.VariableContainer
+}
+
+func (exp *LiteralExpression) Evaluate(globalContext *parser.Context, localContext *parser.Context) (parser.ExpressionResult, error) {
+	return parser.ExpressionResult{Value: &exp.Literal, Return: nil, Brake: nil}, nil
+}

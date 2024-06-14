7a865d51bc4d29f0483352ea40a9c78f9dd94832
Luca Sartore <lucasar.com8@gmail.com>
2024-06-14 19:32:06 +0200

    add one to one operator

--- internal/parser/expressions/one_to_one_operator_expression.go
+++ internal/parser/expressions/one_to_one_operator_expression.go
@@ -1,1 +1,23 @@
 package expressions
+
+import (
+	"cheese-lang/internal/parser"
+	"fmt"
+)
+
+type OneToOneOperator struct {
+	Operator     func(parser.VariableContainer) (parser.VariableContainer, error)
+	VariableName string
+}
+
+func (exp *OneToOneOperator) Evaluate(globalContext *parser.Context, localContext *parser.Context) (parser.ExpressionResult, error) {
+	variable, ok := parser.GetVariable(localContext, globalContext, exp.VariableName)
+	if !ok {
+		return parser.NullExpressionResult, fmt.Errorf("unable to find the variable: %s", exp.VariableName)
+	}
+	result, err := exp.Operator(variable.Value)
+	if err != nil {
+		return parser.NullExpressionResult, err
+	}
+	return parser.ExpressionResult{Value: &result, Return: nil, Brake: nil}, nil
+}

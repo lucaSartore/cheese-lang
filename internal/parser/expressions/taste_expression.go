7ed0a24c8353cb77bbc9d6b70c7b0569dcf6653e
Luca Sartore <lucasar.com8@gmail.com>
2024-06-14 20:45:43 +0200

    Add taste expression

--- internal/parser/expressions/taste_expression.go
+++ internal/parser/expressions/taste_expression.go
@@ -1,3 +1,28 @@
 package expressions
 
-// A TasteExpression is the basic control flow of the language (if)
+import (
+	"cheese-lang/internal/parser"
+	"fmt"
+)
+
+type TasteExpression struct {
+	Condition parser.Expression
+	Code      parser.Expression
+}
+
+func (exp *TasteExpression) Evaluate(globalContext *parser.Context, localContext *parser.Context) (parser.ExpressionResult, error) {
+	result, err := exp.Condition.Evaluate(globalContext, localContext)
+	if err != nil {
+		return parser.NullExpressionResult, err
+	}
+	milk, ok := result.Value.(*parser.MilkVariable)
+	if !ok {
+		return parser.NullExpressionResult, fmt.Errorf(
+			"expected a Milk value inside Taste condition but got %s",
+			result.Value.GetVariableType().String())
+	}
+	if milk.Value {
+		return exp.Code.Evaluate(globalContext, localContext)
+	}
+	return parser.VoidExpressionResult, nil
+}

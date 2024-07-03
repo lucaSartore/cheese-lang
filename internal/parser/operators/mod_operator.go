544a543552673872322927d8ce359f1af6a8953e
Luca Sartore <lucasar.com8@gmail.com>
2024-07-03 08:52:05 +0200

    add modulo operator

--- internal/parser/operators/mod_operator.go
+++ internal/parser/operators/mod_operator.go
@@ -2,8 +2,15 @@ package operators
 
 import (
 	"cheese-lang/internal/parser"
+	"errors"
 )
 
 func ModOperator(v1 parser.VariableContainer, v2 parser.VariableContainer) (parser.VariableContainer, error) {
-	return nil, nil
+	value1, ok1 := v1.(*parser.ParmesanVariable)
+	value2, ok2 := v2.(*parser.ParmesanVariable)
+	if ok1 && ok2 {
+		return &parser.ParmesanVariable{Value: value1.Value % value2.Value}, nil
+	}
+
+	return nil, errors.New("Modulo unsupported for types: " + v1.GetVariableType().String() + ", " + v2.GetVariableType().String())
 }

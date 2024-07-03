f29a1f75f4e986920f0c04dea8963d06371176b0
Luca Sartore <lucasar.com8@gmail.com>
2024-07-03 08:51:52 +0200

    add less equal operator

--- internal/parser/operators/less_equal_operator.go
+++ internal/parser/operators/less_equal_operator.go
@@ -2,8 +2,21 @@ package operators
 
 import (
 	"cheese-lang/internal/parser"
+	"errors"
 )
 
 func lessEqualOperator(v1 parser.VariableContainer, v2 parser.VariableContainer) (parser.VariableContainer, error) {
-	return nil, nil
+	value1, ok1 := v1.(*parser.ParmesanVariable)
+	value2, ok2 := v2.(*parser.ParmesanVariable)
+	if ok1 && ok2 {
+		return &parser.MilkVariable{Value: value1.Value <= value2.Value}, nil
+	}
+
+	value3, ok1 := v1.(*parser.GorgonzolaVariable)
+	value4, ok2 := v2.(*parser.GorgonzolaVariable)
+	if ok1 && ok2 {
+		return &parser.MilkVariable{Value: value3.Value <= value4.Value}, nil
+	}
+
+	return nil, errors.New("Less equal operator unsupported for types: " + v1.GetVariableType().String() + ", " + v2.GetVariableType().String())
 }

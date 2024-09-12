0277733e1b3c09806c12cced184018c4d3563f2c
lucaSartore <lucasar.com8@gmail.com>
2024-09-12 22:13:28 +0200

    Add main program

--- cmd/cheese-lang/main.go
+++ cmd/cheese-lang/main.go
@@ -1,17 +1,72 @@
 package main
-
+	
 import (
-	"fmt"
-
+    "os"
+    "fmt"
 	"cheese-lang/internal/tokenizer"
+	"cheese-lang/internal/parser"
+	"cheese-lang/internal/expressions"
 )
 
+
+
 func main() {
-	s := "\\\\this is a comment\n == = != && ^{    }\t	\n\n \t  -="
-	t, err := tokenizer.Tokenize(s)
-	if err != nil {
-		fmt.Print(err)
-		return
-	}
-	fmt.Println(t)
+   
+    fmt.Println("welcome to cheeselang")
+
+    if len(os.Args) < 2 {
+        fmt.Print("you need to specify a source file in order to run cheeselang")
+        return
+    }
+
+    file := os.Args[1]
+
+    source, err := os.ReadFile(file)
+
+    if err != nil {
+        fmt.Printf("impossible to open the file %v because of error: %v",file, err)
+        return
+    }
+        
+    source_str := string(source)
+    source_str = "{" + source_str + "}"
+
+    tokens, err := tokenizer.Tokenize(source_str)
+
+    if err != nil {
+        fmt.Printf("error in tokenization: %v",err)
+        return
+    }
+    p := parser.MakeParser(tokens)
+
+    parsed_code := p.ParseAnything(true)  
+    
+    if parsed_code.Error != nil {
+        fmt.Printf("unable to parse the code because of error: %v",parsed_code.Error)
+        return
+    }
+
+    local_context := expressions.MakeContext()
+    global_context := expressions.MakeContext()
+    
+    _, err = parsed_code.Expression.Evaluate(&global_context, &local_context)
+
+    if err != nil{
+        fmt.Printf("runtime error: %v",err)
+    }
+    
+    main, success := global_context.GetFunction("main")
+    
+    if !success {
+        fmt.Print("unable to find \"main\" entry point")
+        return
+    }
+    
+    _, err = main.Code.Evaluate(&global_context,&local_context)
+    
+    if err != nil {
+        fmt.Printf("runtime error: %v",err)
+    }
+
+    // print(return_value)
 }
